223e1f7899cdc2c10ea92b263a2e5de8b52c4f4d
mmouterde <martin.mouterde@gmail.com>
2016-05-18 22:07:38 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,49 @@
+var toDom = (function () {
+    var cache = {};
+
+    function parse(string) {
+        var wrapper = document.createElement('div');
+        wrapper.innerHTML = string;
+        return wrapper.firstChild;
+    }
+
+    function readEventHandlers(root, eventHandler) {
+        handleNode(root, eventHandler);
+        Array.prototype.slice.call(root.querySelectorAll("*")).forEach(function (node) {
+            handleNode(node, eventHandler);
+        });
+    }
+
+    function handleNode(node, eventHandler) {
+        for (eventHandlerAttribute in node) {
+            //filter attribute to get eventhandler attributes
+            if (eventHandlerAttribute.indexOf('on') === 0) {
+                //if a template is used as event handler
+                if (node.getAttribute(eventHandlerAttribute) && /^{.*}$/.test(node.getAttribute(eventHandlerAttribute))) {
+                    registerEventHandler(node, eventHandlerAttribute, eventHandler)
+                }
+            }
+        }
+    }
+
+    function registerEventHandler(element, event, eventHandler) {
+        var cacheKey = Math.random().toString(36).substring(7);
+        cache[cacheKey] = {};
+        cache[cacheKey][event.substring(2)] = eventHandler[getFnName(element.getAttribute(event))];
+        element[event] = function (event) {
+            return cache[cacheKey][event.type](event);
+        }
+    }
+
+    function getFnName(template) {
+        return template.substring(1, template.indexOf("("));
+    }
+
+    function toDom(string, eventHandler) {
+        var element = parse(string);
+        readEventHandlers(element, eventHandler);
+        document.getElementById("content").appendChild(element);
+    }
+
+    return toDom;
+})();
